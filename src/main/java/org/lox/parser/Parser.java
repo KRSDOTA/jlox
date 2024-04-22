d536a24ffb019841972689a7b666bbd9d35e9393
KRSProg <meepo0nly2@gmail.com>
2024-04-22 14:46:11 +0100

    added in grammor rules

---
+++ src/main/java/org/lox/parser/Parser.java
@@ -0,0 +1,16 @@
+package org.lox.parser;
+
+import org.lox.scanning.Token;
+
+import java.util.List;
+
+public class Parser {
+
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+}
