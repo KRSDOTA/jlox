5adbd98bd106c5709a29a38eb2b4b141e3af094f
KRSProg <meepo0nly2@gmail.com>
2024-04-24 10:00:37 +0100

    tweaked implementation of recursive descent parser, basically running into stackoverflow issues due to constantly falling through all the conditions and coming back all the way round

--- src/main/java/org/lox/parser/Parser.java
+++ src/main/java/org/lox/parser/Parser.java
@@ -1,26 +1,30 @@
 package org.lox.parser;
 
-import org.lox.abstractsyntaxtree.BinaryExpression;
-import org.lox.abstractsyntaxtree.Expression;
+import org.lox.abstractsyntaxtree.*;
 import org.lox.scanning.Token;
 import org.lox.scanning.TokenType;
+
 import java.util.List;
 
 import static org.lox.scanning.TokenType.*;
 
 public class Parser {
+
+  private final static TokenType[] EQUALITY_OPERATORS = new TokenType[]{BANG_EQUAL, EQUAL_EQUAL};
+  private final static TokenType[] INEQUALITY_OPERATORS = new TokenType[]{GREATER, GREATER_EQUAL, LESS, LESS_EQUAL};
+
   private final List<Token> tokens;
   private int current = 0;
 
-  Parser(List<Token> tokens) {
+  public Parser(List<Token> tokens) {
     this.tokens = tokens;
   }
 
-  private Expression expression() {
+  public Expression expression() {
     Expression expression = comparison();
 
-    while(match(BANG_EQUAL, EQUAL_EQUAL)) {
-      final Token operator = previous();
+    while (matchUnconsumedToken(EQUALITY_OPERATORS)) {
+      final Token operator = consumeToken();
       final Expression right = comparison();
       expression = new BinaryExpression(expression, operator, right);
     }
@@ -31,44 +35,105 @@ public class Parser {
   private Expression comparison() {
     Expression expression = term();
 
-    while(true){
-
+    while (matchUnconsumedToken(INEQUALITY_OPERATORS)) {
+      final Token operator = consumeToken();
+      final Expression right = term();
+      expression = new BinaryExpression(expression, operator, right);
     }
+
+    return expression;
   }
 
-  private boolean match(TokenType... types) {
+  private Expression term() {
+    Expression expression = factor();
+
+    while (matchUnconsumedToken(MINUS, PLUS)) {
+      final Token operator = consumeToken();
+      final Expression right = factor();
+      expression = new BinaryExpression(expression, operator, right);
+    }
+
+    return expression;
+  }
+
+  private Expression factor() {
+    Expression expression = unary();
+
+    while (matchUnconsumedToken(SLASH, STAR)) {
+      final Token operator = consumeToken();
+      final Expression right = unary();
+      expression = new BinaryExpression(expression, operator, right);
+    }
+
+    return expression;
+  }
+
+  private Expression unary() {
+    if (matchUnconsumedToken(BANG, MINUS)) {
+      final Token operator = consumeToken();
+      final Expression right = unary();
+      return new UnaryExpression(operator, right);
+    }
+
+    return primary();
+  }
+
+  private Expression primary() {
+    if (matchUnconsumedToken(FALSE)) {
+      return new LiteralExpression(false);
+    }
+
+    if (matchUnconsumedToken(TRUE)) {
+      return new LiteralExpression(true);
+    }
+
+    if (matchUnconsumedToken(NIL)) {
+      return new LiteralExpression(null);
+    }
+
+    if (matchUnconsumedToken(NUMBER, STRING)) {
+      return new LiteralExpression(consumeToken().literal());
+    }
+
+    if(matchUnconsumedToken(LEFT_PAREN)) {
+      Expression expression = expression();
+//      consume(RIGHT_PAREN, "Expect ')' after expression.");
+      return new GroupingExpression(expression);
+    }
+
+    throw new Error("Lol");
+  }
+
+
+  private boolean matchUnconsumedToken(TokenType... types) {
     for (TokenType type : types) {
-     if(check(type)) {
-       advance();
-       return true;
-     }
+      if (doesNextTokenMatch(type)) {
+        return true;
+      }
     }
     return false;
   }
 
-  private boolean check(TokenType type) {
-    if(isAtEnd()) {
+  private boolean doesNextTokenMatch(TokenType type) {
+    if (isAtEndOfTokenStream()) {
       return false;
     }
-    return peek().tokenType() == type;
+    return tokens.get(current).tokenType() == type;
   }
-  private Token advance() {
-    if(!isAtEnd()) {
+
+  private Token consumeToken() {
+    if (!isAtEndOfTokenStream()) {
       current++;
     }
-    return previous();
+    return mostRecentlyConsumedToken();
   }
 
-  private boolean isAtEnd() {
-    return peek().tokenType() == EOF;
+  private boolean isAtEndOfTokenStream() {
+    return current > tokens.size() - 1 || tokens.get(current).tokenType() == EOF;
   }
 
-  private Token peek() {
-   return tokens.get(current);
+  private Token mostRecentlyConsumedToken() {
+    return tokens.get(current - 1);
   }
-
- private Token previous() {
-   return tokens.get(current - 1);
- }
 
 }
---
+++ src/test/java/Parser/ParserTest.java
@@ -0,0 +1,37 @@
+package Parser;
+
+import org.junit.jupiter.api.Test;
+import org.lox.abstractsyntaxtree.Expression;
+import org.lox.parser.Parser;
+import org.lox.scanning.Token;
+import org.lox.scanning.TokenType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParserTest {
+
+
+
+  @Test
+  void testDoesConstructAstCorrectlyFromTokenStream() {
+    Token plus = new Token(TokenType.PLUS, "+", null, 1);
+    Token multiply = new Token(TokenType.STAR, "*", null, 1);
+    Token five = new Token(TokenType.NUMBER, "5", 5, 1);
+    Token ten = new Token(TokenType.NUMBER, "10", 10, 1);
+    Token three = new Token(TokenType.NUMBER, "3", 3, 1);
+
+    List<Token> tokens = new ArrayList<>();
+    tokens.add(five);
+    tokens.add(plus);
+    tokens.add(three);
+    tokens.add(multiply);
+    tokens.add(ten);
+
+    final Parser parser = new Parser(tokens);
+    Expression expression = parser.expression();
+  }
+
+
+
+}
