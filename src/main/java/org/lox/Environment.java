7996158dba246da89722f1d920b901e6b1d4a994
KRSProg <meepo0nly2@gmail.com>
2024-05-25 07:08:56 +0100

    added in support for environment scoping

--- src/main/java/org/lox/Environment.java
+++ src/main/java/org/lox/Environment.java
@@ -5,9 +5,20 @@ import org.lox.vistor.RuntimeError;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Environment {
   public final Map<String, Object> declarationLookup = new HashMap<>();
+
+  private final Optional<Environment> enclosedScope;
+
+  public Environment() {
+    enclosedScope = Optional.empty();
+  }
+
+  public Environment(Environment enclosedScope) {
+   this.enclosedScope = Optional.of(enclosedScope);
+  }
 
   public void define(Token token, Object value) {
     declarationLookup.put(token.lexeme(), value);
@@ -18,7 +29,9 @@ public class Environment {
       return declarationLookup.get(token.lexeme());
     }
 
-    throw new RuntimeError(token, "undefined variable specified");
+    return enclosedScope
+            .map(environment -> environment.getValue(token))
+            .orElseThrow(() -> new RuntimeError(token, "undefined variable specified"));
   }
 
   public void assign(Token name, Object value) {
@@ -26,6 +39,12 @@ public class Environment {
       declarationLookup.put(name.lexeme(), value);
       return;
     }
+
+    if(enclosedScope.isPresent()) {
+      enclosedScope.get().assign(name, value);
+      return;
+    }
+
    throw new RuntimeError(name, "undefined variable '" + name.lexeme() + "'.");
   }
 }
