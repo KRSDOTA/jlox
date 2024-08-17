8c193493cfb6b712faf39160c686375795493cb9
KRSDOTA <meepo0nly2@gmail.com>
2024-08-17 16:59:14 +0100

    now resolving each type of node

--- src/main/java/org/lox/vistor/Resolver.java
+++ src/main/java/org/lox/vistor/Resolver.java
@@ -2,16 +2,26 @@ package org.lox.vistor;
 
 import org.lox.abstractsyntaxtree.expression.*;
 import org.lox.abstractsyntaxtree.statement.*;
+import org.lox.errorhandler.JLoxErrorHandler;
+import org.lox.errorhandler.JLoxLexerErrorHandler;
+import org.lox.scanning.Token;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * Do a pass of the generated AST and perform variable resolution,
+ * this means we no longer have to dynamically walk the environment tree every time
+ * a variable expression is evaluated. We can pre-calculate the number of "hops" required from the current
+ * scope to get to the correct place of resolution.
+ */
 public class Resolver implements ExpressionVisitor<Void>, StatementVisitor<Void> {
 
     private final Interpreter interpreter;
     private final Stack<Map<String, Boolean>> scopes = new Stack<>();
+    private final JLoxErrorHandler errorHandler = new JLoxLexerErrorHandler();
 
     public Resolver(Interpreter interpreter) {
         this.interpreter = interpreter;
@@ -19,11 +29,14 @@ public class Resolver implements ExpressionVisitor<Void>, StatementVisitor<Void>
 
     @Override
     public Void visitBinaryExpr(BinaryExpression binaryExpression) {
+        resolve(binaryExpression.getLeftHandExpression());
+        resolve(binaryExpression.getRightHandExpression());
         return null;
     }
 
     @Override
     public Void visitGroupingExpr(GroupingExpression groupingExpression) {
+        resolve(groupingExpression.getGroupedExpression());
         return null;
     }
 
@@ -34,52 +47,94 @@ public class Resolver implements ExpressionVisitor<Void>, StatementVisitor<Void>
 
     @Override
     public Void visitUnaryExpr(UnaryExpression unaryExpression) {
+        resolve(unaryExpression.getRightHandExpression());
         return null;
     }
 
     @Override
     public Void visitConditionalExpr(ConditionalExpression conditionalExpression) {
+        resolve(conditionalExpression.getExpression());
+        resolve(conditionalExpression.getThenBranch());
+        if (conditionalExpression.getElseBranch() != null) {
+            resolve(conditionalExpression.getElseBranch());
+        }
         return null;
     }
 
     @Override
     public Void visitVariableExpr(VariableExpression variableExpression) {
+        if (!scopes.isEmpty() && scopes.peek().get(variableExpression.getToken().lexeme()) == Boolean.FALSE) {
+            errorHandler.reportError(variableExpression.getToken(), "Can't read local variable in its own initialiser");
+        }
+        resolveLocal(variableExpression, variableExpression.getToken());
         return null;
+    }
+
+    private void resolveLocal(Expression expression, Token token) {
+        for (int i = scopes.size() - 1; i >= 0; i--) {
+            if (scopes.get(i).containsKey(token.lexeme())){
+                interpreter.resolve(expression, scopes.size() - 1 -i);
+                return;
+            }
+        }
     }
 
     @Override
     public Void visitAssignmentExpr(AssignmentExpression assignmentExpression) {
+        resolve(assignmentExpression.getValue());
+        resolveLocal(assignmentExpression, assignmentExpression.getToken());
         return null;
     }
 
     @Override
     public Void visitLogicalExpression(LogicalExpression logicalExpression) {
+        resolve(logicalExpression.getLeft());
+        resolve(logicalExpression.getRight());
         return null;
     }
 
     @Override
     public Void visitCallExpression(CallExpression callExpression) {
+        resolve(callExpression.getCallee());
+        callExpression.getArguments().forEach(this::resolve);
         return null;
     }
 
     @Override
     public Void visitExpressionStatement(ExpressionStatement expressionStatement) {
+        resolve(expressionStatement);
         return null;
     }
 
     @Override
     public Void visitPrintStatement(PrintStatement printStatement) {
+        resolve(printStatement.getStatement());
         return null;
     }
 
     @Override
     public Void visitVariableStatement(VariableStatement variableStatement) {
        declare(variableStatement.getTokenName());
-       if(variableStatement.getExpression() != null){
+       if (variableStatement.getExpression() != null) {
            resolve(variableStatement.getExpression());
        }
        define(variableStatement.getTokenName());
        return null;
+    }
+
+    private void declare(Token tokenName) {
+        if(scopes.isEmpty()) {
+            return;
+        }
+        Map<String, Boolean> scope = scopes.peek();
+        scope.put(tokenName.lexeme(), false);
+    }
+
+    private void define(Token tokenName) {
+        if(scopes.isEmpty()) {
+            return;
+        }
+        scopes.peek().put(tokenName.lexeme(), true);
     }
 
     @Override
@@ -112,21 +167,42 @@ public class Resolver implements ExpressionVisitor<Void>, StatementVisitor<Void>
 
     @Override
     public Void visitIfStatement(IfStatement ifStatement) {
+        resolve(ifStatement.getCondition());
+        resolve(ifStatement.getThenBranch());
+        if (ifStatement.getThenBranch() != null) {
+           resolve(ifStatement.getElseBranch());
+        }
         return null;
     }
 
     @Override
     public Void visitWhileStatement(WhileStatement whileStatement) {
+        resolve(whileStatement.getCondition());
+        resolve(whileStatement.getStatement());
         return null;
     }
 
     @Override
     public Void visitFunctionDeclaration(FunctionDeclaration functionDeclaration) {
+        declare(functionDeclaration.getName());
+        define(functionDeclaration.getName());
+        resolveFunction(functionDeclaration);
         return null;
+    }
+
+    private void resolveFunction(FunctionDeclaration functionDeclaration) {
+        beginScope();
+        functionDeclaration.getParams().forEach(param -> {
+            declare(param);
+            define(param);
+        });
+        resolve(functionDeclaration.getBody());
+        endScope();
     }
 
     @Override
     public Void visitReturnStatement(ReturnStatement returnStatement) {
+        resolve(returnStatement);
         return null;
     }
 }
